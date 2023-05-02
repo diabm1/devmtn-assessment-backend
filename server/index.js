7426da1cab36320b9878344d7d6ca600c50c2dbf
diabm1 <diabm1855@gmail.com>
2023-05-02 13:16:28 -0400

    adding some more comments

--- server/index.js
+++ server/index.js
@@ -17,13 +17,16 @@ const {
   getFortune,
 } = require("./controller");
 
+// Routes for fetching compliments and fortunes
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
 
+// Routes for CRUD operations on gratitude entries
 app.post("/api/gratitude", postGratitude);
 app.get("/api/gratitude/", getGratitudeEntries);
 // app.get("/api/gratitude/date", getGratitudeEntriesByDate);
 app.put("/api/gratitude/:id", putGratitudeEntry);
 app.delete("/api/gratitude/:id", deleteGratitudeEntry);
 
+// Start the server on port 4004
 app.listen(4004, () => console.log("Server running on 4004"));
